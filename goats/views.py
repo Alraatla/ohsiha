3b3cf2d8e4b4cf5fc3b12dc9d7ed142485076094
Aleksi Raatala <aleksi.raatala@tuni.fi>
2020-03-27 18:28:09 +0200

    Creation and reading works

--- goats/views.py
+++ goats/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Goat
-# from .forms import GoatForm
+from .forms import GoatForm
 
 def list_goats(request):
     goats = Goat.objects.all()
     return render(request, 'goats.html', {'goats': goats})
 
-# def create_goat(request):
-#     form = GoatForm(request.POST or None)
-#
-#     if form.is_valid():
-#         form.save()
-#         return redirect('list_goats')
-#
-#     return render(request, 'goats-form.html', {'form': form})
+def create_goat(request):
+    form = GoatForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_goats')
+
+    return render(request, 'goats-form.html', {'form': form})
 
 # def update_goat(request, id):
 #     goat = Goat.objects.get(id=id)
