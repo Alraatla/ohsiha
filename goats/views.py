82ba1b721a350fe0026dfd003ff9ce63fdeb80a3
Aleksi Raatala <aleksi.raatala@tuni.fi>
2020-04-02 23:47:32 +0300

    Trying to get the goat ownership working

--- goats/views.py
+++ goats/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Goat
 from .forms import GoatForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 def list_goats(request):
-    goats = Goat.objects.all()
+
+    logger.error(request.user)
+    goats = Goat.objects.filter(owner = request.user)
+    logger.error(goats)
+
+    # goats = Goat.objects.all()
+    # goats.delete()
     return render(request, 'goats.html', {'goats': goats})
 
 def create_goat(request):
@@ -11,6 +20,8 @@ def create_goat(request):
 
     if form.is_valid():
         form.save()
+        Goat.objects.all().order_by('id')[0].set_owner(request.user)
+
         return redirect('list_goats')
 
     return render(request, 'goats-form.html', {'form': form})
